14b89f6f530ffee76d8e0bce5c7cd5be56c366e5
Taiju Aoki <aokitaiju0513@gmail.com>
2021-01-08 00:55:48 +0900

    add clojars username

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject too-hot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.blue0513 too-hot "1.0.0"
+  :description "A very simple Fahrenheit to Celsius converter."
+  :url "http://github.com/blue0513/too-hot"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
