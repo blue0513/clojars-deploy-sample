2f1855c76f6688547cf2415b15f079a62916ef7a
Taiju Aoki <aokitaiju0513@gmail.com>
2021-01-08 00:50:33 +0900

    add logic

--- src/too_hot/core.clj
+++ src/too_hot/core.clj
@@ -1,6 +1,11 @@
 (ns too-hot.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn celsius
+  "Converts a temperature in Fahrenheit to Celsius."
+  [f]
+  (* (- f 32) 5/9))
+
+(defn fahrenheit
+  "Converts a temperature in Celsius to Fahrenheit."
+  [c]
+  (+ (* c 9/5) 32))
