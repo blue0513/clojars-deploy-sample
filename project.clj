22c23a5b38111f84f57b57cc12445812a07b9612
Taiju Aoki <aokitaiju0513@gmail.com>
2021-01-08 00:59:11 +0900

    update a bit

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.blue0513 too-hot "1.0.0"
+(defproject org.clojars.blue0513/too-hot "1.0.0"
   :description "A very simple Fahrenheit to Celsius converter."
   :url "http://github.com/blue0513/clojars-deploy-sample"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
