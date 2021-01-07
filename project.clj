e25b5c39c57b5150006c8ab1ba2d102cb33f3e31
Taiju Aoki <aokitaiju0513@gmail.com>
2021-01-08 00:57:50 +0900

    fix url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.blue0513 too-hot "1.0.0"
   :description "A very simple Fahrenheit to Celsius converter."
-  :url "http://github.com/blue0513/too-hot"
+  :url "http://github.com/blue0513/clojars-deploy-sample"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
